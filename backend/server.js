88c6be4f1a1fe059e8c611b4b1143d0a84dd1478
cyberzed313 <zafer.arifi290@gmail.com>
2025-03-15 14:40:10 +0430

    Add Get and Post for the backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
 const PORT = 5000;
 
@@ -8,13 +9,39 @@ app.use(cors());
 app.use(express.json()); //To parse json requests
 
 
+// Sample task list (temporary in-memory data)
+let tasks = [
+  { id: 1, title: "Learn React", description: "Build a Task Manager app" },
+  { id: 2, title: "Learn Node.js", description: "Build an API" },
+];
+
 //Test route
-app.get('/', (req, res) => {
-    res.send("Hello from the backend");
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
+
+app.post("/tasks", (req, res) => {
+  const { title, description } = req.body;
+
+  //validate the input
+  if (!title || !description) {
+    return res.status(400).json({ message: "Title and Description are required." });
+  }
+
+  //Create new task
+  const newTask = {
+    id: tasks.length + 1,
+    title,
+    description,
+  };
+
+
+  tasks.push(newTask); // Add to tasks array
+  res.status(201).json(newTask); // Send response
 });
 
 
-//Start Sever
+//Start the Server
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
